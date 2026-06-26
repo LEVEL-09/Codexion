de183820afe51e837b21ea2b16445681db1ca526
mkhoubaz <hichamohicham651@gmail.com>
2026-06-26 06:20:21 +0100

    chore(.gitignore): add name of program

--- src/codexion.h
+++ src/codexion.h
@@ -6,9 +6,41 @@
 /*   By: mkhoubaz <mkhoubaz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/24 20:23:45 by mkhoubaz          #+#    #+#             */
-/*   Updated: 2026/06/24 20:25:33 by mkhoubaz         ###   ########.fr       */
+/*   Updated: 2026/07/03 16:43:58 by mkhoubaz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#pragma once
+#ifndef CODEXION_H
+# define CODEXION_H
 
+# include <pthread.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+# include <limits.h>
+
+typedef struct s_coder	t_coder;
+typedef struct s_dongle	t_dongle;
+
+struct s_coder
+{
+	int	time_to_burnout;
+	int	time_to_compile;
+	int	time_to_debug;
+	int	time_to_refactor;
+	int	number_of_compiles_required;
+};
+
+struct s_dongle
+{
+	char			*name;
+	int				dongle_cooldown;
+	pthread_mutex_t	dongle_mutex;
+};
+
+int		check_argument(int argc, char *argv[]);
+t_coder		coder_init(char *argv[]);
+t_dongle	dongle_init(char *argv[]);
+long		ft_atoi(const char *str);
+
+#endif
