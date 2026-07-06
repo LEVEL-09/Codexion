6a510ad5aa1f1a17e8a083d7b76c89a92e0b0e5c
Mkhoubaz <hichamohicham651@gmail.com>
2026-07-06 18:57:23 +0100

    feat: create coders and dongles

--- src/codexion.c
+++ src/codexion.c
@@ -6,32 +6,61 @@
 /*   By: mkhoubaz <mkhoubaz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/24 18:40:18 by mkhoubaz          #+#    #+#             */
-/*   Updated: 2026/07/03 16:42:54 by mkhoubaz         ###   ########.fr       */
+/*   Updated: 2026/07/06 18:52:51 by mkhoubaz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <codexion.h>
+#include "codexion.h"
+
+t_coder	**create_coders(int number_of_coder)
+{
+	int			i;
+	t_coder		**coders;
+
+	coders = malloc(sizeof(t_coder *) * number_of_coder);
+	if (!coders)
+		return (NULL);
+	i = 0;
+	while (i < number_of_coder)
+	{
+		coders[i] = malloc(sizeof(t_coder));
+		if (!coders[i])
+			return (NULL);
+		coders[i]->coder_id = i;
+		i++;
+	}
+	return (coders);
+}
+
+t_dongle	**create_dongle(int number_of_dongle)
+{
+	int			i;
+	t_dongle	**dongles;
+
+	dongles = malloc(sizeof(t_dongle *) * number_of_dongle);
+	if (!dongles)
+		return (NULL);
+	i = 0;
+	while (i < number_of_dongle)
+	{
+		dongles[i] = malloc(sizeof(t_dongle));
+		if (dongles[i])
+			return (NULL);
+		dongles[i]->dongle_id = i;
+		i++;
+	}
+	return (dongles);
+}
 
 int	main(int argc, char *argv[])
 {
-	int			i;
-	long		number_of_coders;
-	t_coder		*coders_struct;
-	t_dongle	*dongle_struct;
+	int			number_of_coders;
+	t_coder		**coders;
+	t_dongle	**dongles;
 
 	if (!check_argument(argc, argv))
 		return (fprintf(stderr, "Error: Invalid arguments\n"));
-
-	number_of_coders = ft_atoi(argv[1]);
-
-	coders_struct = malloc(sizeof(t_coder) * number_of_coders);
-	dongle_struct = malloc(sizeof(t_dongle) * number_of_coders);
-
-	i = 0;
-	while (i < number_of_coders)
-	{
-		coders_struct[i] = coder_init(argv);
-		dongle_struct[i] = dongle_init(argv);
-		i++;
-	}
+	number_of_coders = (int)ft_atoi(argv[1]);
+	coders = create_coders(number_of_coders);
+	dongles = create_dongle(number_of_coders);
 }
--- src/codexion.h
+++ src/codexion.h
@@ -6,7 +6,7 @@
 /*   By: mkhoubaz <mkhoubaz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/24 20:23:45 by mkhoubaz          #+#    #+#             */
-/*   Updated: 2026/07/06 06:28:41 by mkhoubaz         ###   ########.fr       */
+/*   Updated: 2026/07/06 18:53:15 by mkhoubaz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,33 +21,35 @@
 
 typedef struct s_coder	t_coder;
 typedef struct s_dongle	t_dongle;
-typedef struct s_heap 	t_heap;
+typedef struct s_heap	t_heap;
 
 struct s_coder
 {
-	int	time_to_burnout;
-	int	time_to_compile;
-	int	time_to_debug;
-	int	time_to_refactor;
-	int	number_of_compiles_required;
+	int				coder_id;
+	int				time_to_burnout;
+	int				time_to_compile;
+	int				time_to_debug;
+	int				time_to_refactor;
+	int				number_of_compiles_required;
 };
 
 struct s_dongle
 {
+	int				dongle_id; /* For Debug */
 	int				dongle_cooldown;
 	pthread_mutex_t	dongle_mutex;
 };
 
 struct s_heap
 {
-	t_coder	**array;
-	int		capacity;
-	int		size;
+	t_coder			**array;
+	int				capacity;
+	int				size;
 };
 
-int			check_argument(int argc, char *argv[]);
-t_coder		coder_init(char *argv[]);
-t_dongle	dongle_init(char *argv[]);
-long		ft_atoi(const char *str);
+int					check_argument(int argc, char *argv[]);
+t_coder				coder_init(char *argv[]);
+t_dongle			dongle_init(char *argv[]);
+long				ft_atoi(const char *str);
 
 #endif
